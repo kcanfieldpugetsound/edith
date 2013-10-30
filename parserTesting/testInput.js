539dafaac3109d45f61eaf82fde21eaf7a8faeca
Jon Youmans <jyoumans@pugetsound.edu>
2013-10-29 20:52:30 -0700

    added parsertesting folder

---
+++ parserTesting/testInput.js
@@ -0,0 +1,5 @@
+{
+{"function name" : "jump", "Image Name": "mario.jpg", "sound name": "flyby.mp3"}
+{"function name" : "movR", "Image Name": "mario.jpg", "sound name": "null"}
+{"function name" : "movL", "Image Name": "mario.jpg", "sound name": "null"}
+}
