a6f59d45c21bb985b34c0267ce5b43598df60b25
Eric Lund <ericlund@Erics-iMac.local>
2013-10-31 14:48:38 -0700

    Cleaned up the testing files

--- parserTesting/testInput.js
+++
@@ -1,5 +0,0 @@
-{
-{"function name" : "jump", "Image Name": "mario.jpg", "sound name": "flyby.mp3"}
-{"function name" : "movR", "Image Name": "mario.jpg", "sound name": "null"}
-{"function name" : "movL", "Image Name": "mario.jpg", "sound name": "null"}
-}
