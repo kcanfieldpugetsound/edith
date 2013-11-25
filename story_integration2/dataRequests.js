b98db697b3ff5e846641c988aca87cb43e3dfc26
brobertsUPS <broberts@pugetsound.edu>
2013-11-25 09:16:40 -0800

    Creates integration file

---
+++ story_integration2/dataRequests.js
@@ -0,0 +1,102 @@
+var theUserName;
+var theObjectName;
+var theObjectMethodName;
+
+
+	function getProject(userName)//get the project from the database using the userName
+	{
+		if (userName=="")//if userName is empty then there is no project assosciated
+		{
+			theUserName = "";
+			return;
+		}
+		
+		if (window.XMLHttpRequest)//requesting from IE7, Firefox, Chrome, Opera, Safari
+		{
+			xmlhttp=new XMLHttpRequest();
+		}
+		else//requesting from IE6 or IE5
+		{
+			xmlhttp=new ActiveXObject("Microsoft.XMLHTTP");
+		}
+		
+		xmlhttp.onreadystatechange = function()
+		{
+			if(xmlhttp.readyState==4 && xmlhttp.status==200)
+			{
+				theUserName = xmlhttp.responseText;
+			}
+		}
+		
+		xmlhttp.open("GET", "display.php?userName=" + userName, true);//request a display of the user files with the userName
+		xmlhttp.send();//send the request to server php page
+	}
+	
+	function getObject(objectName)//using current project get the object from the database using the objectName
+	{
+		if (objectName=="")//if objectName is empty then there is no object assosciated
+		{
+			theObjectName = "";
+			return;
+		}
+		
+		if (window.XMLHttpRequest)//requesting from IE7, Firefox, Chrome, Opera, Safari
+		{
+			xmlhttp=new XMLHttpRequest();
+		}
+		else//requesting from IE6 or IE5
+		{
+			xmlhttp=new ActiveXObject("Microsoft.XMLHTTP");
+		}
+		
+		xmlhttp.onreadystatechange = function()
+		{
+			if(xmlhttp.readyState==4 && xmlhttp.status==200)
+			{
+				theObjectName = xmlhttp.responseText;
+			}
+		}
+		
+		xmlhttp.open("GET", "load.php?id=" + objectName, true);//request an object of the user files with the objectName
+		xmlhttp.send();//send the request to server php page
+	}
+	
+	function getVEMethod(objectMethodName)//using current object get the VE method for that object (as JSON??????)*******************************
+	{
+		if (objectMethodName=="")//if objectMethodName is empty then there is no method assosciated
+		{
+			theObjectMethodName = "";
+			return;
+		}
+		
+		if (window.XMLHttpRequest)//requesting from IE7, Firefox, Chrome, Opera, Safari
+		{
+			xmlhttp=new XMLHttpRequest();
+		}
+		else//requesting from IE6 or IE5
+		{
+			xmlhttp=new ActiveXObject("Microsoft.XMLHTTP");
+		}
+		
+		xmlhttp.onreadystatechange = function()
+		{
+			if(xmlhttp.readyState==4 && xmlhttp.status==200)
+			{
+				theObjectMethodName = xmlhttp.responseText;
+			}
+		}
+		
+		xmlhttp.open("GET", "display.php?objectMehodName=" + objectMethodName, true);//request the updated VE method for the object
+		xmlhttp.send();//send the request to server php page
+	}
+		
+	/*
+        $.get( "http://localhost/functions/load.php", 'project_kitten' , function( data ) {//A test get request
+          alert(data);
+        })
+
+        .fail(function() {
+                alert('bad request');
+        });
+		
+	*/
